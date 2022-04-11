bf7913d4d380ba9102c4c645fb73877de536a7c0
Jonathan Deamer <deamer.jonathan+github@gmail.com>
2022-04-11 19:03:11 +0100

    Hackily hardcoding file paths

--- nbn-host-feed.py
+++ nbn-host-feed.py
@@ -6,7 +6,7 @@ import boto3
 import os
 import json
 
-os.environ["AWS_CONFIG_FILE"] = "config"
+os.environ["AWS_CONFIG_FILE"] = "/home/private/nbn-host-feed/config"
 
 # Set up feed
 nbn_host_profile = 'https://newbooksnetwork.com/hosts/profile/112f8337-847b-411d-b237-0171df1fb217'
@@ -70,7 +70,7 @@ fg.rss_file('miranda.xml') # Write the RSS feed to a file
 
 # Send file to S3
 
-file_name=os.path.basename('miranda.xml')
+file_name=os.path.basename('/home/private/nbn-host-feed/miranda.xml')
 bucket='nbn-host-feed'
 
 s3 = boto3.client('s3')
