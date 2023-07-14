2f5942943ceb69a7024107b65279edc9c9c30866
Jonathan Deamer <deamer.jonathan+github@gmail.com>
2023-07-14 13:29:27 +0100

    Performance and error handling improvements

--- nbn-host-feed.py
+++ nbn-host-feed.py
@@ -24,48 +24,57 @@ fg.link(href=nbn_host_profile,rel='alternate')
 fg.language('en')
 fg.load_extension('base')	
 
+with requests.Session() as s:
+    # Get episode page links
+    page = s.get(nbn_host_profile, timeout=5)
+    if page.status_code != 200:
+        print(f"Failed to get page: {nbn_host_profile}")
+    else:
+        soup = BeautifulSoup(page.text, 'lxml')
+        episode_cards=soup.find_all('div', class_='episode-card')
+        episode_links=[]
 
-# Get episode page links
-page = requests.get(nbn_host_profile)
-soup = BeautifulSoup(page.text, 'html.parser')
-episode_cards=soup.find_all('div', class_='episode-card')
-episode_links=[]
+        for each in episode_cards:
+            anchors=each.find_all('a')
+            url = anchors[1].get('href')
+            correct_url = url.encode('latin-1').decode('utf-8')
+            episode_links.append(correct_url)
+        # Get content from episode page and add episode to feed
+        for each in reversed(episode_links):
+            fe = fg.add_entry()
+            fe.id(each)
+            fe.guid(each,permalink=True)
+            fe.link(href=each,rel='alternate')
+            fe.link(href=feed_url,rel='self')
+            fe.author(name='Miranda Melcher')
+            fe.contributor( name='Miranda Melcher')
+            print(each)
+            page = s.get(each, timeout=5)
+            if page.status_code != 200:
+                print(f"Failed to get page: {each}")
+                continue
+            try:
+                soup = BeautifulSoup(page.text, 'lxml')
+            except Exception as e:
+                print(f"Failed to parse page: {each}. Error: {str(e)}")
+                continue
+            iframes = soup.find_all('iframe')
+            megaphone_url = iframes[1].get('src')
+            print(megaphone_url)
+            megaphone_id = re.search('([A-Z])\w+', megaphone_url)
+            print(megaphone_id)
+            mp3_url = 'https://dcs.megaphone.fm/' + megaphone_id.group() + '.mp3'
+            fe.enclosure(url=mp3_url,type='audio/mpeg') # set podcast audio url
+            book_title = soup.find('h1')
+            book_author = soup.find('h4')
+            fe.title(book_title.string + ' (' + book_author.string + ')') # set episode title
+            description = soup.find('div', class_='episode')
+            fe.description(description=str(description),isSummary=True)
+            fe.content(content=str(description),type='text/html')
 
-for each in episode_cards:
-	anchors=each.find_all('a')
-	episode_links.append((anchors[1].get('href')))
-
-# Get content from episode page and add episode to feed
-for each in episode_links:
-	fe = fg.add_entry()
-	fe.id(each)
-	fe.guid(each,permalink=True)
-	fe.link(href=each,rel='alternate')
-	fe.link(href=feed_url,rel='self')
-	fe.author(name='Miranda Melcher')
-	fe.contributor( name='Miranda Melcher')
-	print(each)
-	page = requests.get(each)
-	soup = BeautifulSoup(page.text, 'html.parser')
-	iframes = soup.find_all('iframe')
-	megaphone_url = iframes[1].get('src')
-	print(megaphone_url)
-	megaphone_id = re.search('([A-Z])\w+', megaphone_url)
-	print(megaphone_id)
-	mp3_url = 'https://dcs.megaphone.fm/' + megaphone_id.group() + '.mp3'
-	fe.enclosure(url=mp3_url,type='audio/mpeg') # set podcast audio url
-	book_title = soup.find('h1')
-	book_author = soup.find('h4')
-	fe.title(book_title.string + ' (' + book_author.string + ')') # set episode title
-	description = soup.find('div', class_='episode')
-	fe.description(description=str(description),isSummary=True)
-	fe.content(content=str(description),type='text/html')
-
-	
-	script_tags = soup.find_all('script')
-	pub_date_container=json.loads(script_tags[3].string)
-	fe.published(pub_date_container['@graph'][0]['datePublished'])
-
+            script_tags = soup.find_all('script')
+            pub_date_container=json.loads(script_tags[3].string)
+            fe.published(pub_date_container['@graph'][0]['datePublished'])
 
 rssfeed  = fg.rss_str(pretty=True) # Get the RSS feed as string
 fg.rss_file('/home/private/nbn-host-feed/miranda.xml') # Write the RSS feed to a file
