b8d8da66d51ab1cb52ffa88e9cbfed44047f5205
Jonathan Deamer <deamer.jonathan+github@gmail.com>
2023-07-14 14:31:43 +0100

    Added historic episode handling instead of overwrite

--- nbn-host-feed.py
+++ nbn-host-feed.py
@@ -5,6 +5,7 @@ import re
 import boto3
 import os
 import json
+import feedparser
 
 # Set the AWS configuration file environment variable
 os.environ["AWS_CONFIG_FILE"] = "/home/private/nbn-host-feed/config"
@@ -13,21 +14,43 @@ os.environ["AWS_CONFIG_FILE"] = "/home/private/nbn-host-feed/config"
 nbn_host_profile = 'https://newbooksnetwork.com/hosts/profile/112f8337-847b-411d-b237-0171df1fb217'
 feed_url='https://nbn-host-feed.s3.eu-west-2.amazonaws.com/miranda.xml'
 
+# Parse the existing feed
+d = feedparser.parse(feed_url)
+
+# Extract the entries
+existing_entries = d.entries
+
 # Create an instance of FeedGenerator
 fg = FeedGenerator()
 
 # Set properties for the feed
 fg.title('Miranda Melcher on New Books Network')
 fg.logo('https://nbn-host-feed.s3.eu-west-2.amazonaws.com/miranda-nbn.png')
-fg.author( name='Miranda Melcher')
-fg.contributor( name='Miranda Melcher')
+fg.author(name='Miranda Melcher')
+fg.contributor(name='Miranda Melcher')
 fg.description(description='Miranda\'s interviews with authors from across New Books Network channels.')
 fg.id(nbn_host_profile)
 fg.ttl(ttl=1440)
-fg.link(href=feed_url,rel='self')
-fg.link(href=nbn_host_profile,rel='alternate')
+fg.link(href=feed_url, rel='self')
+fg.link(href=nbn_host_profile, rel='alternate')
 fg.language('en')
-fg.load_extension('base')	
+fg.load_extension('base')
+
+
+# Add historic entries from existing feed, as these may not be available on the profile page
+for entry in existing_entries:
+    # Create a new entry in the feed
+    fe = fg.add_entry()
+
+    # Set the details of the entry
+    fe.id(entry.id)
+    fe.guid(entry.id, permalink=True)
+    fe.link({'href': entry.link, 'rel': 'alternate'})
+    fe.author(name='Miranda Melcher')
+    fe.contributor(name='Miranda Melcher')
+    fe.description(description=entry.description)
+    fe.content(content=entry.description, type='text/html')
+    fe.published(entry.published)
 
 # Start a requests session for persistent HTTP connections
 with requests.Session() as s:
@@ -39,12 +62,12 @@ with requests.Session() as s:
         # Parse the HTML of the host profile page
         soup = BeautifulSoup(page.text, 'lxml')
         # Find all episode cards
-        episode_cards=soup.find_all('div', class_='episode-card')
-        episode_links=[]
+        episode_cards = soup.find_all('div', class_='episode-card')
+        episode_links = []
 
         # For each episode card, find the episode URL and add it to the list
         for each in episode_cards:
-            anchors=each.find_all('a')
+            anchors = each.find_all('a')
             url = anchors[1].get('href')
             # Correct the URL encoding
             correct_url = url.encode('latin-1').decode('utf-8')
@@ -55,11 +78,10 @@ with requests.Session() as s:
             # Initialize a new feed entry
             fe = fg.add_entry()
             fe.id(each)
-            fe.guid(each,permalink=True)
-            fe.link(href=each,rel='alternate')
-            fe.link(href=feed_url,rel='self')
+            fe.guid(each, permalink=True)
+            fe.link(href=each, rel='alternate')
             fe.author(name='Miranda Melcher')
-            fe.contributor( name='Miranda Melcher')
+            fe.contributor(name='Miranda Melcher')
             print(each)
             # Get the episode page
             page = s.get(each, timeout=5)
@@ -81,29 +103,31 @@ with requests.Session() as s:
             print(megaphone_id)
             mp3_url = 'https://dcs.megaphone.fm/' + megaphone_id.group() + '.mp3'
             # Set the podcast audio URL
-            fe.enclosure(url=mp3_url,type='audio/mpeg')
+            fe.enclosure(url=mp3_url, type='audio/mpeg')
             book_title = soup.find('h1')
             book_author = soup.find('h4')
             # Set the episode title
             fe.title(book_title.string + ' (' + book_author.string + ')')
             description = soup.find('div', class_='episode')
             # Set the episode description and content
-            fe.description(description=str(description),isSummary=True)
-            fe.content(content=str(description),type='text/html')
+            fe.description(description=str(description), isSummary=True)
+            fe.content(content=str(description), type='text/html')
 
             # Extract the publication date from the script tags
             script_tags = soup.find_all('script')
-            pub_date_container=json.loads(script_tags[3].string)
+            pub_date_container = json.loads(script_tags[3].string)
             fe.published(pub_date_container['@graph'][0]['datePublished'])
 
 # Get the RSS feed as a string and write it to an XML file
-rssfeed  = fg.rss_str(pretty=True)
-fg.rss_file('/home/private/nbn-host-feed/miranda.xml')
+rssfeed = fg.rss_str(pretty=True)
+fg.rss_file('miranda.xml')
+
+#fg.rss_file('/home/private/nbn-host-feed/miranda.xml')
 
 # Prepare to upload the XML file to S3
-file_name='/home/private/nbn-host-feed/miranda.xml'
-object_name=os.path.basename('/home/private/nbn-host-feed/miranda.xml')
-bucket='nbn-host-feed'
+file_name = '/home/private/nbn-host-feed/miranda.xml'
+object_name = os.path.basename('/home/private/nbn-host-feed/miranda.xml')
+bucket = 'nbn-host-feed'
 
 # Create an S3 client
 s3 = boto3.client('s3')
